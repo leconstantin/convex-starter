a9fc3eba41e91edf0a6591f91b2cbc0956ead877
Leo Constantin <lcon69184@gmail.com>
2025-07-27 13:27:24 +0200

    simple fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import { GeistMono } from "geist/font/mono";
-import { GeistSans } from "geist/font/sans";
-import type { Metadata } from "next";
-import "./globals.css";
-import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
-import { Analytics } from "@vercel/analytics/next";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import './globals.css';
+import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
+import { Analytics } from '@vercel/analytics/next';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
+import { ConvexClientProvider } from './ConvexClientProvider';
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Don't Forget",
+    template: "%s |Don't Forget",
     default: "Don't Forget : Tasks and Issues Tracker",
   },
-  description: "A task and issue tracker build to solve all your problems.",
+  description: 'A task and issue tracker build to solve all your problems.',
 };
 
 export default function RootLayout({
